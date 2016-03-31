98ff1d153c6f1bf11351ca3f1694cad3bdbc23ba
EC2 Default User <ec2-user@ip-10-0-0-187.ap-northeast-1.compute.internal>
2016-03-31 09:48:19 +0000

    “add new fil

---
+++ command.sh
@@ -0,0 +1,8 @@
+echo "# aws ec2 describe-instances"
+aws ec2 describe-instances
+echo
+echo "# aws iam list-users"
+aws iam list-users
+echo
+echo "# aws ec2 describe-security-groups"
+aws ec2 describe-security-groups
---
+++ getconfig.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+IFS=$','
+
+while read row; do
+  customerlist=(`echo "${row}"`)
+  # create directory
+  dir=/var/config/aws/${customerlist[3]}; [ ! -e ${dir} ] && sudo mkdir -p ${dir} && sudo chmod 777 ${dir}
+  
+  # logrotation
+  file=${dir}/config
+  MAX_LEVEL=6
+  MV=mv
+  level=${MAX_LEVEL}
+  while [ ${level} -gt 1 ]; do
+    dst="${file}.${level}.log"
+    level=$(expr ${level} - 1)
+    src="${file}.${level}.log"
+    [ -f "${src}" ] && ${MV} "${src}" "${dst}"
+  done
+  dst="${file}.1.log"
+  src="${file}.log"
+  [ -f "${src}" ] && ${MV} "${src}" "${dst}"
+
+  # get configuration
+  export AWS_ACCESS_KEY_ID=${customerlist[0]}
+  export AWS_SECRET_ACCESS_KEY=${customerlist[1]}
+  export AWS_DEFAULT_REGION=${customerlist[2]}
+  export AWS_DEFAULT_OUTPUT=text
+  /home/ec2-user/command.sh > ${dir}/config.log
+
+  # compare config file
+  diff -u ${dir}/config.1.log ${dir}/config.log > ${dir}/diff.log
+
+  # send result to slack
+  WEBHOOKURL="https://hooks.slack.com/services/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"
+  CHANNEL="#general"
+  BOTNAME="diff result"
+  FACEICON=":raising_hand:"
+  TMPFILE=$(mktemp)
+  cat ${dir}/diff.log | tr '\n' '\\' | sed 's/\\/\\n/g' > ${TMPFILE}
+  WEBMESSAGE=$(cat ${TMPFILE})
+  curl -s -S -X POST --data-urlencode "payload={ \
+    \"channel\": \"${CHANNEL}\", \
+    \"username\": \"${BOTNAME}\", \
+    \"icon_emoji\": \"${FACEICON}\", \
+    \"text\": \"${WEBMESSAGE}\" \
+    }" ${WEBHOOKURL} >/dev/null
+  if [ -f "${TMPFILE}" ] ; then
+    rm -f ${TMPFILE}
+  fi
+  exit 0
+done < customerlist.csv
